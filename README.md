54f9aa208dbc9435386c599fcd3b3746c21e4722
Edgar Carrillo <ecarrillo046@gmail.com>
2021-09-20 17:02:27 -0700

    Add starter files and folders

---
+++ README.md
@@ -0,0 +1,11 @@
+# What is this?
+
+Create multiple menu items in the <nav> of your html document with nested menu options.
+Menu options will appear in the form of a drop down menu that appears and disappears.
+All that you need to do is create it in html and the javascript will do the rest.
+
+# Installation
+
+'$ npm i dropDown-expand-contract --save'
+
+# How to use
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Drop Down',
+      template: 'src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
