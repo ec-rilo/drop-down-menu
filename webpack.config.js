1e555fb317b2172012807d0694c4d0ecf38aee33
Edgar Carrillo <ecarrillo046@gmail.com>
2021-09-22 14:48:52 -0700

    Add images rule

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
